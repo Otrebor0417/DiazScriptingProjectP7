bed3bc4a7ae564875931c0f196fdbf50f355771e
Otrebor0417 <145072312+Otrebor0417@users.noreply.github.com>
2026-03-10 15:19:17 -0700

    b fvb dvdv

---
+++ P7DiazScriptingProject/Assets/Scripts/GetAxis/AxisRawExample.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class AxisRawExample : MonoBehaviour
+{
+    public float range;
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float xPos = h * range;
+
+        transform.position = new Vector3(xPos, 2f, 0);
+        Debug.Log("Value Returned: " + h.ToString("F2"));
+    }
+}
